f8c4380c5bf664d2b529698e95c431d8b8a2b21c
saranyamaity <saranyamaity2000@gmail.com>
2022-07-13 12:25:22 +0530

    prevoius comments removed for readFile and Write file , and little optimised calling for Save and FetchAll

--- models/product.js
+++ models/product.js
@@ -6,23 +6,20 @@ module.exports = class Product {
     this.title = title;
     this.imgLink = imgLink;
   }
-  save() {
-    const targetPath = path.join(global.__basedir, "data", "products.json");
-    fs.readFile(targetPath, (err, fileContent) => {
-      let products = [];
-      if (!err && fileContent.length > 0) products = JSON.parse(fileContent);
-      products.push(this); // if I had used normal function this would refer to the local this of the function , so better use arrow function instead;
-      fs.writeFile(targetPath, JSON.stringify(products), (err) => {
-        console.error("Some Problem has Occured! While writing new data!");
-      });
-    });
-  }
   static fetchAll(cb) {
     const targetPath = path.join(global.__basedir, "data", "products.json");
     fs.readFile(targetPath, (err, fileContent) => {
-      if (!err && fileContent.length > 0) products = JSON.parse(fileContent); // if file content length is 0 then parse dosen't work! 
-      
-      else cb([]); // implementing like this for overcomming the async problem!
+      if (!err && fileContent.length > 0) cb(JSON.parse(fileContent));
+      else cb([]);
+    });
+  }
+  save() {
+    const targetPath = path.join(global.__basedir, "data", "products.json");
+    Product.fetchAll((products) => {
+      products.push(this);
+      fs.writeFile(targetPath, JSON.stringify(products), err => {
+        if (err) console.log(err);
+      });
     });
   }
 };
