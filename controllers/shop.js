7c533ab3d99e200f3fa92a467027d268bf64dad9
saranyamaity <saranyamaity2000@gmail.com>
2022-07-17 22:50:03 +0530

    added ORDER section -front end

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,25 +1,30 @@
 const Product = require("../models/product");
 
-// path -> /
+// path -> / (GET)
 exports.getHomePage = (req, res) => {
     Product.fetchAll((products) => {
         res.render("shop/index", { pageTitle: "Shop", prods: products });
     });
 };
 
-// path -> /products 
+// path -> /products  (GET)
 exports.getProducts = (req, res) => {
     Product.fetchAll((products) => {
         res.render("shop/product-list", { pageTitle: "Products", prods: products });
     });
 };
 
-// path -> /cart
+// path -> /orders (GET)
+exports.getOrders = (req, res) => {
+    res.render("shop/orders", { pageTitle: "Orders" });
+}
+
+// path -> /cart (GET)
 exports.getCart = (req, res) => {
     res.render("shop/cart", {pageTitle: "Cart"});
 };
 
-// path -> /checkout
+// path -> /checkout (GET)
 exports.getCheckout = (req, res) => {
     res.render("shop/checkout", { pageTitle: "Checkout"});
 };
